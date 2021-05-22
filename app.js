9224f332c4c6df3792b294d2caa096174eb60577
samflab <kasturimeh@gmail.com>
2021-05-22 20:10:19 +0530

    linked with translation api

--- app.js
+++ app.js
@@ -2,14 +2,29 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-if (btnTranslate) {
-    btnTranslate.addEventListener("click", clickHandler);
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+    return serverURL + "?text=" + text;
 }
-if (outputDiv) {
-   // outputDiv.innerText = "Masudha Meher";
-    console.log(outputDiv);
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("Server gone mad! please leave it alone");
 }
 
 function clickHandler() {
-    outputDiv.innerText = "translated: "+ txtInput.value;
+    var inputText = txtInput.value;
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translated = json.contents.translated;
+            outputDiv.innerText = translated;
+        })
+        .catch(errorHandler);
 };
+
+if (btnTranslate) {
+    btnTranslate.addEventListener("click", clickHandler);
+}
+
